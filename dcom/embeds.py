7cfb83f53522a706618d1777990a1d5823410df0
Emre Yilmaz <mail@emreyilmaz.me>
2018-12-16 15:51:23 +0300

    Move embeds to a  different submodule for a better organizing in the structure

---
+++ dcom/embeds.py
@@ -0,0 +1,24 @@
+from discord import Embed
+
+def get_help():
+    embed = Embed(
+        color=0x2ecc71,  # green
+    )
+    embed.add_field(
+        name="$upvote",
+        value="Upvotes a post with the specified voting weight."
+              "```$upvote <post_url> <vote_weight_in_percent>```",
+        inline=False,
+    )
+    embed.add_field(
+        name="$vp",
+        value="Shows the current voting power.",
+        inline=False,
+    )
+    embed.add_field(
+        name="$help",
+        value="Shows this message.",
+        inline=False,
+    )
+
+    return embed
