56ebc63f12d5a9413e81dd4a18ce37945cca3670
Emre Yilmaz <mail@emreyilmaz.me>
2018-12-17 23:55:36 +0300

    Version bump to 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dcom',
-    version='0.0.4',
+    version='0.0.5',
     packages=['dcom'],
     url='https://github.com/emre/dcom',
     license='MIT',
